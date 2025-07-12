e823ea643a587d66197e8270d687e3569bc77602
Asif Akram <akramasif506@gmail.com>
2025-07-12 14:38:48 +0000

    why login in Sell tb

--- src/app/listings/new/listing-form.tsx
+++ src/app/listings/new/listing-form.tsx
@@ -57,7 +57,7 @@ export function ListingForm() {
   const [isSuggesting, setIsSuggesting] = useState(false);
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { toast } = useToast();
-  const { user } = useAuth();
+  const { user, loading: isAuthLoading } = useAuth();
   
   const form = useForm<ClientListingSchema>({
     resolver: zodResolver(clientListingSchema),
@@ -188,6 +188,14 @@ export function ListingForm() {
   const selectedCategory = form.watch('category');
   const productImageRef = form.register("productImage");
 
+
+  if (isAuthLoading) {
+    return (
+      <div className="flex justify-center items-center p-20">
+        <Loader2 className="h-12 w-12 animate-spin text-primary" />
+      </div>
+    );
+  }
 
   if (!user) {
     return (
