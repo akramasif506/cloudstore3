2d553aa2b106f0077d9b3888685d4e69f39a8904
Asif Akram <akramasif506@gmail.com>
2025-07-12 12:58:28 +0000

    frustrated

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -31,13 +31,15 @@ export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-6 flex items-center space-x-2">
-          <Leaf className="h-6 w-6 text-primary" />
-          <div>
-              <span className="font-bold font-headline text-lg">CloudStore</span>
-              <p className="text-xs text-muted-foreground -mt-1">A Akram Product</p>
-          </div>
-        </Link>
+        <div className="mr-4 flex items-center">
+            <Link href="/" className="flex items-center space-x-2">
+                <Leaf className="h-6 w-6 text-primary" />
+                <div>
+                    <span className="font-bold font-headline text-lg">CloudStore</span>
+                    <p className="text-xs text-muted-foreground -mt-1">A Akram Product</p>
+                </div>
+            </Link>
+        </div>
         
         <nav className="flex items-center space-x-6 text-sm font-medium">
           <Link href="/" className="transition-colors hover:text-foreground/80 text-foreground">
